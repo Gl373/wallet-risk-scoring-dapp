7dacb5aee8f1a01e547f03f3883b26d40a1e1895
Gl373 <jihiglete@yahoo.com>
2025-12-08 14:41:50 +0100

    :heavy_plus_sign: added wagmi and viem

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import{ WagmiConfig } from 'wagmi'
+import { wagmiConfig } from './wagmiConfig.js'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <WagmiConfig config={wagmiConfig}>
+      <App />
+    </WagmiConfig>
   </StrictMode>,
 )
